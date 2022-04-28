02189830a4edcd01e8654c1461e928158f28dd21
Adrian Suciu <adrian.suciu@analog.com>
2022-04-28 20:02:45 +0300

    port to gr-3.10 - wip
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- src/dmm.cpp
+++ src/dmm.cpp
@@ -63,7 +63,7 @@ using namespace libm2k::context;
 DMM::DMM(struct iio_context *ctx, Filter *filt, ToolMenuItem *toolMenuItem,
 	 QJSEngine *engine, ToolLauncher *parent)
 	: Tool(ctx, toolMenuItem, new DMM_API(this), "Voltmeter", parent),
-	  ui(new Ui::DMM), signal(boost::make_shared<signal_sample>()),
+      ui(new Ui::DMM), signal(std::make_shared<signal_sample>()),
 	  manager(iio_manager::get_instance(ctx, filt->device_name(TOOL_DMM))),
 	  m_m2k_context(m2kOpen(ctx, "")),
 	m_m2k_analogin(m_m2k_context->getAnalogIn()),
